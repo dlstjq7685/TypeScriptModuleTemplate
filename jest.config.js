403bdc4c70ac3242ab552671103a191c093b2453
dlstjq7685 <dlstjq7685@gmail.com>
2020-08-14 18:30:03 +0900

    fix

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
       "<rootDir>/src"
     ],
     "testMatch": [
-      "/__tests__/**/*.+(ts|tsx|js)",
-      "/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
+      "**/__tests__/**/*.+(ts|tsx|js)",
+      "**/?(*.)+(spec|test).+(ts|tsx|js)"
+      ],
     "transform": {
       "^.+\\.(ts|tsx)$": "ts-jest"
     },
